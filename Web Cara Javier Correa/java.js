3f797b20747f99ed9e707931f68df853a3978ebf
javiwilde <55982565+javiwilde@users.noreply.github.com>
2019-11-12 02:13:15 +0100

    Add files via upload

---
+++ Web Cara Javier Correa/java.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+
+    TweenMax.to(".Titulo", 1, {
+        color: "#96F6D0",
+    });
+
+    TweenMax.to(".TextoIntroduccion", 1, {
+        color: "#96F6D0",
+    });
+
+}
